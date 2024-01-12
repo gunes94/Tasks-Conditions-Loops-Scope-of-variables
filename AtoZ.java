297fcd3cce04a08ebbb1a1ec844a3751b0f58366
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 17:43:12 +0200

    Create AtoZ.java

---
+++ AtoZ.java
@@ -0,0 +1,9 @@
+//Направете програма, която отпечатва буквите от A до Z, като използвате цикъл.
+
+public class Exercise_02_AToZ {
+    public static void main(String[] args) {
+        for(char letter='A'; letter<='Z'; letter++) {
+            System.out.printf("%C%n", letter);
+        }
+    }
+}
